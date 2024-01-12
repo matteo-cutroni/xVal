finally fixed gradient issue

model.py
@@ -1,25 +1,26 @@
 import torch
 from torch import nn
 
-
-
-
 class Numformer(nn.Module):
     def __init__(
-            self,
-            vocab_size, #same hyperparameters as paper code
-            d_model=160,
-            nhead=5,
-            num_layers=6,
-            dim_feedforward=640,
-            dropout=0.1,
-            activation=nn.GELU(),
-            layer_norm_eps=1e-05,
-            batch_first=True,
-            norm_first=True,
-            context_length=224):
+        self,
+        vocab_size,
+        d_model=768,
+        nhead=6,
+        num_layers=6,
+        dim_feedforward=3072,
+        dropout=0.1,
+        activation=nn.GELU(),
+        layer_norm_eps=1e-05,
+        batch_first=True,
+        norm_first=True,
+        transformer_bias=False,
+        numhead_bias=True,
+        context_length=1024,
+        is_causal=False,
+    ):
         super().__init__()
-        encoder_layer = nn.TransformerEncoderLayer(
+        encoder = nn.TransformerEncoderLayer(
             d_model=d_model,
             nhead=nhead,
             dim_feedforward=dim_feedforward,
@@ -27,42 +28,38 @@ class Numformer(nn.Module):
             activation=activation,
             layer_norm_eps=layer_norm_eps,
             batch_first=batch_first,
-            norm_first=norm_first)
-        
-        self.encoder = nn.TransformerEncoder(
-            encoder_layer=encoder_layer,
-            num_layers=num_layers,
-            enable_nested_tensor=False)
-        
-        #text needs to be embedded and given the positional information
-        self.token_embed_table = nn.Embedding(vocab_size, d_model)
-        self.position_embed_table = nn.Embedding(context_length, d_model)
-        
-        #token head and num head with hidden layer dimension = 4*embedding dimension
+            norm_first=norm_first,
+            # bias=transformer_bias,
+        )
+        self.encoder_stack = nn.TransformerEncoder(
+            encoder_layer=encoder, num_layers=num_layers, enable_nested_tensor=False
+        )
+        self.token_embed = nn.Embedding(vocab_size, d_model)
+        self.position_embed = nn.Embedding(context_length, d_model)
         self.lm_head = nn.Sequential(
-            nn.Linear(d_model, dim_feedforward),
-            activation,
-            nn.Linear(dim_feedforward, vocab_size)
+            nn.Linear(d_model, dim_feedforward, bias=transformer_bias),
+            nn.GELU(),
+            nn.Linear(dim_feedforward, vocab_size, bias=transformer_bias),
         )
         self.num_head = nn.Sequential(
-            nn.Linear(d_model, dim_feedforward),
-            activation,
-            nn.Linear(dim_feedforward, 1)
+            nn.Linear(d_model, dim_feedforward, bias=numhead_bias),
+            nn.GELU(),
+            nn.Linear(dim_feedforward, 1, bias=numhead_bias),
         )
-
+        self.is_causal = is_causal
 
     def forward(self, x, x_num):
         batch_dim, time_dim = x.shape
 
-        tok_emb = self.token_embed_table(x)
+        x = self.token_embed(x)
         #pos_emb takes as input a tensor with values from 0 to time_dim-1 (time_dim == context_length)
-        pos_emb = self.position_embed_table(torch.arange(time_dim).to(tok_emb.device))
+        pos_emb = self.position_embed(torch.arange(time_dim).to(x.device))
         
-        x = tok_emb + pos_emb
+        x = x + pos_emb
         #h_emb = h_text*h_num
         x = x*x_num.unsqueeze(-1)
 
-        x = self.encoder(x)
+        x = self.encoder_stack(x)
         logits = self.lm_head(x)
         num = self.num_head(x)
         return logits, num

train.py
@@ -33,10 +33,10 @@ mask_token_id = tokenizer.mask_token_id
 num_token_id = tokenizer.convert_tokens_to_ids('[NUM]')
 
 def collator(batch):
-    x = [torch.Tensor(sample["input_ids"]) for sample in batch]
+    x = [torch.tensor(sample["input_ids"]) for sample in batch]
     x = pad_sequence(x, batch_first=True, padding_value=pad_token_id)
 
-    x_num = [torch.Tensor(sample["numbers"]) for sample in batch]
+    x_num = [torch.tensor(sample["numbers"]) for sample in batch]
     x_num = pad_sequence(x_num, batch_first=True, padding_value=1.)
 
     y = x.clone()
@@ -45,7 +45,6 @@ def collator(batch):
     mask = torch.rand(x.shape) < mask_prob
     x[mask] = mask_token_id
     x_num[mask] = 1.
-
     
     return {'x': x, 'x_num': x_num, 'y':y, 'y_num':y_num}
 
@@ -65,7 +64,7 @@ opt = optim.Adam(m.parameters(), lr=learning_rate)
 loss_mlm = nn.CrossEntropyLoss()
 loss_num = nn.MSELoss()
 
-log_interval = 500
+log_interval = 32
 tot_loss = []
 mlm_loss = []
 num_loss = []
@@ -75,12 +74,8 @@ for e in tqdm(range(epochs)):
 
         x, x_num = batch['x'].to(device), batch['x_num'].to(device)
 
-        x.requires_grad = True
-        x_num.requires_grad = True
 
         logit_pred, num_pred = m(x, x_num)
-        logit_pred.requires_grad = True
-        num_pred.requires_grad = True
 
 
         l_mlm = loss_mlm(logit_pred.view(-1, logit_pred.size(-1)).to(device),
@@ -89,10 +84,8 @@ for e in tqdm(range(epochs)):
         num_mask = batch['y']==num_token_id
         l_num = loss_num( num_pred[num_mask].to(device),
                     batch["y_num"][num_mask].view(-1,1).to(device))
-        l_mlm.requires_grad = True
-        l_num.requires_grad = True
+
         loss = l_mlm+l_num
-        print(logit_pred.grad_fn)
         opt.zero_grad()
 
         loss.backward()
@@ -103,6 +96,6 @@ for e in tqdm(range(epochs)):
         num_loss.append(l_num.item())
 
         
-                #ogni tanto stampa loss di questo batch
+        #ogni tanto stampa loss di questo batch
         if batch_idx % log_interval == 0:
             print(f'Train Epoch: {e} [{batch_idx * len(x)}/{len(train_loader.dataset)} ({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss.data.item():.4f}')